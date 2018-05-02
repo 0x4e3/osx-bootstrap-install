39bbc2c54d5cf49ab8b49b40be84e1e5bf0fb30d
Alexander Lebedev <lebedev@0x4e3.ru>
2018-05-03 02:51:17 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,7 +61,7 @@ install_command_line_tools() {
     CLT=$(softwareupdate -l | grep -B 1 -E "Command Line Tools" | awk -F'*' '/^ +\*/ {print $2}' | sed 's/^ *//' | head -n1)
     while [ -n "$CLT" ]; do
         softwareupdate -i "$CLT" --verbose
-        CLT=$(softwareupdate -l | grep -B 1 -E "Command Line Tools" | awk -F'*' '/^ +\*/ {print $2}' | sed 's/^ *//' | head -n1)
+        $CLT=$(softwareupdate -l | grep -B 1 -E "Command Line Tools" | awk -F'*' '/^ +\*/ {print $2}' | sed 's/^ *//' | head -n1)
     done
     # PROD=$(softwareupdate -l | grep "\*.*Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
     
